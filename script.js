2d6b0cd888de77f7f0260ffe9234285897b96376
Storm OBryant <vmarie1997@gmail.com>
2022-06-10 09:03:00 -0500

    fixed js

---
+++ script.js
@@ -0,0 +1,24 @@
+//arrays are list objects
+//make a classroom
+'use strict'
+let classroom = [];
+
+//we need students
+let student = ["Curtrick", "Zion", "Tyler", "Michael", "Storm", "Jordan", "Lindsey", "Lizzy"];
+
+//add students to classroom
+
+function addStudents(studentsArray, classroomArray){
+    let greeting = "Hello World! My name is";
+    if (studentsArray.includes("Lizzy")){
+        studentsArray.pop("Lizzy");
+    }
+    for(let i = 0; i < studentsArray.length; i++){
+            classroomArray.push(studentsArray[i]);
+            console.log(greeting + " " +classroomArray[i]);
+        }
+}
+
+//once students in classroom, greet the class
+addStudents(student, classroom);
+
